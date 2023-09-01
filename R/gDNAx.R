acf050b4227d935d4d8bd306c6a1e1c965ac48df
beacalvo <beacalvo96@gmail.com>
2023-09-01 19:16:35 +0200

    Added package level man page ?gDNAx

---
+++ R/gDNAx.R
@@ -0,0 +1,41 @@
+#' gDNAx: diagnostics for assessing genomic DNA contamination in RNA-seq data
+#' 
+#' The gDNAx package provides diagnostics for assessing genomic DNA
+#' contamination in RNA-seq data, as well as plots representing these
+#' diagnostics. Moreover, the package can be used to get an insight into
+#' the strand library protocol used and, in case of strand-specific libraries,
+#' the strandedness of the data. Furthermore, it provides functionality to
+#' filter out reads of potential gDNA origin.
+#' 
+#' The main functions are:
+#'
+#' \itemize{
+#' \item \code{\link{identifyStrandMode}} - identify \code{strandMode} (strandedness) in RNA-seq data samples based on the proportion of reads aligning to the same or opposite strand as transcripts in the annotations
+#' \item \code{\link{gDNAdx}} - calculate diagnostics for assessing the presence of genomic DNA in RNA-seq data over a subset of the alignments in the input BAM files
+#' \item \code{\link{getDx}} and code{\link{plot}} - get and plot statistics on genomic DNA contamination levels, respectively
+#' \item \code{\link{filterBAMtxFlag}} and code{\link{filterBAMtx}} - filter alignments in a BAM file using criteria based on a transcriptome annotation
+#' }
+#' 
+#' For detailed information on usage, see the package vignette, by typing
+#' \code{vignette("gDNAx")}.
+#' 
+#' All questions and bug reports should be posted to the Bioconductor Support 
+#' Site:
+#' 
+#' \url{https://support.bioconductor.org}
+#'
+#' The code of the development version of the package is available at the
+#' GitHub repository:
+#'
+#' \url{https://github.com/functionalgenomics/gDNAx}
+#' 
+#' 
+#' 
+#' 
+#' @docType package
+#' @name gDNAx-package
+#' @aliases gDNAx-package
+#' @aliases gDNAx
+#' @keywords package
+
+NULL
