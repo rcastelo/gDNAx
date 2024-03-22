26ede4bd89abb9b318d8d042585afaaaaf0dbf41
Robert Castelo <robert.castelo@upf.edu>
2024-03-22 10:31:08 +0100

    Fixing documentation and unit tests

--- inst/unitTests/test_strandedness.R
+++ inst/unitTests/test_strandedness.R
@@ -10,75 +10,64 @@ bamfiles <- LiYu22subsetBAMfiles()
 pdat <- LiYu22phenoData(bamfiles)
 
 
-test_input_errors_identifyStrandMode <- function() {
+test_input_errors_strandedness <- function() {
 
     # Retrieving BAM files
     # bamfiles <- LiYu22subsetBAMfiles()
     
     ## bamfiles
     bamfiles2 <- c("invented_sample.bam")
-    checkException(identifyStrandMode(bamfiles2, txdb, singleEnd=FALSE),
+    checkException(strandedness(bamfiles2, txdb, singleEnd=FALSE),
                     "testing bamfiles input error")
     
     ## txdb
     txdb2 <- "invented_txdb_packagename"
-    checkException(identifyStrandMode(bamfiles, txdb2, singleEnd=FALSE),
+    checkException(strandedness(bamfiles, txdb2, singleEnd=FALSE),
                     "testing txdb input error")
     
     ## singleEnd
-    checkException(identifyStrandMode(bamfiles, txdb, singleEnd="no"),
+    checkException(strandedness(bamfiles, txdb, singleEnd="no"),
                     "testing singleEnd input error")
     
     ## stdChrom
-    checkException(identifyStrandMode(bamfiles, txdb, singleEnd=FALSE,
-                        stdChrom="yes"), "testing stdChrom input error")
+    checkException(strandedness(bamfiles, txdb, singleEnd=FALSE, stdChrom="yes"),
+                   "testing stdChrom input error")
     
-    ## yieldSize
-    checkException(identifyStrandMode(bamfiles, txdb, singleEnd=FALSE,
-                        yieldSize = "100"), "testing yieldSize input error")
-    checkException(identifyStrandMode(bamfiles, txdb, singleEnd=FALSE,
-                        yieldSize = NA), "testing yieldSize input error")
-    checkException(identifyStrandMode(bamfiles, txdb, singleEnd=FALSE,
-                        yieldSize = 100), "testing yieldSize input error")
 }
 
-test_output_identifyStrandMode <- function() {
+test_output_strandedness <- function() {
 
     # Retrieving BAM files
     # bamfiles <- LiYu22subsetBAMfiles()
     
-    suppressWarnings(strandM <- identifyStrandMode(bamfiles[1:2], txdb,
-                                                    singleEnd=FALSE))
+    suppressWarnings(strness <- strandedness(bamfiles[1:2], txdb, singleEnd=FALSE))
     
-    checkTrue(is(strandM, "list"))
-    checkEquals(length(strandM), 2)
-    checkTrue(is(strandM$Strandedness, "matrix"))
+    checkTrue(is(strness, "data.frame"))
     
     # All % and frag length values should be numeric and between 0 and 1
-    checkTrue(is(unlist(as.vector(strandM$Strandedness[,1:3])), "numeric"))
-    checkTrue(all(strandM$Strandedness[,1:3] >= 0 & 
-                    strandM$Strandedness[,1:3] <= 1))
+    checkTrue(is(unlist(strness[, 1:3]), "numeric"))
+    checkTrue(all(unlist(strness[, 1:3]) >= 0 & unlist(strness[, 1:3]) <= 1))
     
     # strandMode1 + strandMode2 + ambig should add 1
-    checkEquals(as.vector(rowSums(strandM$Strandedness[,1:3])),
+    checkEquals(as.vector(rowSums(strness[, 1:3])),
                 rep(1, length(bamfiles[1:2])), tolerance = 1.0e-3)
 }
 
-test_decideStrandMode <- function() {
+test_classifyStrandMode <- function() {
 
     ## unstranded: NA
     strbysm <- data.frame("strandMode1" = 0.3, "strandMode2" = 0.45,
                           "ambig" = 0.25)
-    checkTrue(is.na(gDNAx::classifyStrandMode(strbysm)))
+    checkTrue(is.na(classifyStrandMode(strbysm)))
     
     ## strandMode2
     strbysm <- data.frame("strandMode1" = 0.01, "strandMode2" = 0.94,
                           "ambig" = 0.05)
-    checkIdentical(as.integer(gDNAx::classifyStrandMode(strbysm)), 2L)
+    checkIdentical(as.integer(classifyStrandMode(strbysm)), 2L)
     
     ## strandMode1
     strbysm <- data.frame("strandMode1" = 0.91, "strandMode2" = 0.08,
                           "ambig" = 0.01)
-    checkIdentical(as.integer(gDNAx::classifyStrandMode(strbysm)), 1L)
+    checkIdentical(as.integer(classifyStrandMode(strbysm)), 1L)
     
 }
